ff65bfc1a8900132816c36ccb64bf239288b87c2
sazzadshakhbd <sazzadshakhbd@gmail.com>
2024-11-24 18:42:34 +0600

    middleware: define url depends on user login or logout

--- my-app/src/middleware.js
+++ my-app/src/middleware.js
@@ -1,13 +1,17 @@
 import { NextResponse } from 'next/server'
  
+const user = false;
+
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
-    if (request.nextUrl.pathname.startsWith('/about')) {
+    
+    if (!user) {
         return NextResponse.redirect(new URL('/login', request.url))
     }
+    return NextResponse.next();
 }
  
 // See "Matching Paths" below to learn more
-// export const config = {
-// matcher: ['/blogs', '/services', '/blogs/specialblogs'],
-// }
+export const config = {
+matcher: ['/blogs', '/services', '/blogs/specialblogs'],
+}
