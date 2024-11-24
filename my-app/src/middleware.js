932dde886f210a91263af41e7c935793051e7cf3
sazzadshakhbd <sazzadshakhbd@gmail.com>
2024-11-24 18:32:29 +0600

    middleware: redirect login page without matcher

--- my-app/src/middleware.js
+++ my-app/src/middleware.js
@@ -2,10 +2,12 @@ import { NextResponse } from 'next/server'
  
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
-  return NextResponse.redirect(new URL('/login', request.url))
+    if (request.nextUrl.pathname.startsWith('/about')) {
+        return NextResponse.redirect(new URL('/login', request.url))
+    }
 }
  
 // See "Matching Paths" below to learn more
-export const config = {
-matcher: ['/blogs', '/services', '/blogs/specialblogs'],
-}
+// export const config = {
+// matcher: ['/blogs', '/services', '/blogs/specialblogs'],
+// }
