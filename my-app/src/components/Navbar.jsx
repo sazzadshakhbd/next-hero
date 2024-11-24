5bfe38db37843923a967744a6a46b5c7c4cae959
sazzadshakhbd <sazzadshakhbd@gmail.com>
2024-11-24 09:37:32 +0600

    after click active url name using usePathname

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
+'use client'
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
-
 export default function Navbar() {
     const links = [
         {title: "About", path: "/about"},
@@ -9,12 +10,13 @@ export default function Navbar() {
         {title: "Portfolio", path: "/portfolio"},
         {title: "Services", path: "/services"},
     ]
+    const pathName = usePathname();
   return (
     <nav className="bg-teal-800 py-4 px-6 flex justify-between items-center text-white">
       <h6 className="text-3xl">Next <span className="text-cyan-300">Hero</span></h6>
       <ul className="flex justify-between items-center space-x-4">
         {
-            links?.map((link)=><Link key={link.path} href={link.path}>{link.title}</Link>)
+            links?.map((link)=><Link className={`${pathName === link.path && "bg-white text-black px-3"}`} key={link.path} href={link.path}>{link.title}</Link>)
         }
       </ul>
     </nav>
