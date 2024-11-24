faccb7fe0383314e77d278ac7efe51fa4db63693
sazzadshakhbd <sazzadshakhbd@gmail.com>
2024-11-24 07:55:27 +0600

    add Heading component in our page.js file on our app folder

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,8 +1,11 @@
+import Heading from "@/components/Heading";
+
 export default function Home() {
   return (
     <div>
       <h3>Hello, Next Hero</h3>
       <p>This is new heading</p>
+      <Heading></Heading>
     </div>
   );
 }
