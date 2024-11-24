9996ff3fc0cdd5ba06c8f9b08dc03ebf2714d657
sazzadshakhbd <sazzadshakhbd@gmail.com>
2024-11-24 15:09:11 +0600

    categories details page declare conditionaly using catch all segments

--- my-app/src/app/categories/[...features]/page.jsx
+++ my-app/src/app/categories/[...features]/page.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
-export default function CategoriesDetailsPage() {
+export default function CategoriesDetailsPage({params}) {
+    if(params.features.length == 3) {
+        return <div>{params.features[2]}</div>
+    }
+    if(params.features.length == 2) {
+        return <div>{params.features[1]}</div>
+    }
   return (
     <div>CategoriesDetailsPage</div>
   )
