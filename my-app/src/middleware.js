92bab24a0d68b28e301ab5957641f2d45c2c9873
sazzadshakhbd <sazzadshakhbd@gmail.com>
2024-11-24 18:23:33 +0600

    middleware: multiple  page redirection

--- my-app/src/middleware.js
+++ my-app/src/middleware.js
@@ -2,10 +2,10 @@ import { NextResponse } from 'next/server'
  
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
-  return NextResponse.rewrite(new URL('/login', request.url))
+  return NextResponse.redirect(new URL('/login', request.url))
 }
  
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: '/blogs',
+matcher: ['/blogs', '/services', '/blogs/specialblogs'],
 }
