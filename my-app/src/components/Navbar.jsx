c99f452e5f85794a6a527485039ffd509baaeda3
sazzadshakhbd <sazzadshakhbd@gmail.com>
2024-11-24 09:05:18 +0600

    desing navbar and url add for about using Link tag

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,11 +1,14 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Navbar() {
   return (
-    <nav className="bg-teal-800 p-4 text-white">
-      <h6>Logo</h6>
-      <ul>
-        <li>About</li>
+    <nav className="bg-teal-800 py-4 px-6 flex justify-between items-center text-white">
+      <h6 className="text-3xl">Next <span className="text-cyan-300">Hero</span></h6>
+      <ul className="flex justify-between items-center space-x-4">
+        <li>
+            <Link href={'/about'} target="_blank">About</Link>
+        </li>
         <li>Service</li>
         <li>Contact</li>
       </ul>
