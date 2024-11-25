d82752ac2cbb350b44ae5c215465176cf1bed6be
sazzadshakhbd <sazzadshakhbd@gmail.com>
2024-11-25 21:21:39 +0600

    middleware: create cookies and apply conditionaly route login page user excess or not

--- my-app/src/middleware.js
+++ my-app/src/middleware.js
@@ -6,8 +6,9 @@ const user = true;
 export function middleware(request) {
     
 
-    const cookies = request.cookies.get('token')
-    if (!cookies) {
+    const cookies = request.cookies.get('token');
+    const coo = 'next-superhero';
+    if (!cookies || cookies.value !== coo) {
         return NextResponse.redirect(new URL('/login', request.url))
     }
     return NextResponse.next();
