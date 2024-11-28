5fbae0c980a604392057555fd72a90165eb176b6
sazzadshakhbd <sazzadshakhbd@gmail.com>
2024-11-28 09:04:36 +0600

    data fetching: show just 20 data of hundred using slice

--- my-app/src/app/post/page.jsx
+++ my-app/src/app/post/page.jsx
@@ -11,10 +11,10 @@ const page =  async () => {
   console.log(postData)
   return (
     <div>
-      <h1 className='text-center text-teal-500 font-bold '>All Posts</h1>
+      <h1 className='text-center text-teal-500 font-bold m-5'>All Posts</h1>
       <div className='grid grid-cols-4 p-6'>
         {
-          postData.map(({id, title, body})=> 
+          postData.slice(0,20).map(({id, title, body})=> 
           <div className='border-2 p-5 m-3'>
             <h3 className='text-red-400'>{id} Title: {title}</h3>
             <p>{body}</p>
